84018f2a7c0bca086058a18f5699289b52fd6a3f
Lucas Mello <lucas@bitka.com.br>
2022-03-03 22:54:32 -0300

    fix rate limiter

--- server/config/express.js
+++ server/config/express.js
@@ -14,11 +14,11 @@ const passport = require('./passport');
 
 const app = express();
 
-// set up rate limiter: maximum of five requests per minute
+// set up rate limiter: maximum of fifty requests per minute
 var RateLimit = require('express-rate-limit');
-var limiter = new RateLimit({
+var limiter = RateLimit({
     windowMs: 1 * 60 * 1000, // 1 minute
-    max: 5
+    max: 50
 });
 
 // apply rate limiter to all requests
